4f8fa5173810c8bbba92c4d6c6f30408fe856f40
--get-all <yimoxia@outlook.com>
2022-07-22 09:11:45 +0800

    基本服务器完成

--- src/bilibili_danmaku_server/danmaku/views.py
+++ src/bilibili_danmaku_server/danmaku/views.py
@@ -1,7 +1,11 @@
+import asyncio
 from cgitb import text
+import json
+from xml.dom.minidom import Attr
 from django.shortcuts import render
 from django.http import HttpResponse
-from bilibili_api import video, sync, interactive_video
+from bilibili_api import Danmaku, DmFontSize, DmMode, video, sync, interactive_video
+from numpy import isin
 
 # Create your views here.
 
@@ -9,9 +13,9 @@ def index(request):
     return render(request, "danmaku/index.html")
 
 def danmaku(request, aid, page):
-    try:
+    if True:#try:
         v = video.Video(aid=aid)
-        danmakus = sync(v.get_danmakus(page))
+        danmakus = asyncio.run(v.get_danmakus(page))
         dm_list = []
         for dm in danmakus:
             dm_dict = {}
@@ -19,22 +23,31 @@ def danmaku(request, aid, page):
             dm_dict["dm_time"] = dm.dm_time
             dm_dict["send_time"] = dm.send_time
             dm_dict["crc32_id"] = dm.crc32_id
-            dm_dict["mid"] = dm.crack_uid()
+            try:
+                dm_dict["mid"] = dm.crack_uid()
+            except AttributeError:
+                dm_dict["mid"] = 0
             dm_dict["color"] = dm.color
             dm_dict["weight"] = dm.weight
             dm_dict["id"] = dm.id
             dm_dict["id_str"] = dm.id_str
             dm_dict["action"] = dm.action
-            dm_dict["mode"] = dm.mode
-            dm_dict["font_size"] = dm.font_size
+            if isinstance(dm.mode, DmMode):
+                dm_dict["mode"] = dm.mode.value
+            else:
+                dm_dict["mode"] = dm.mode
+            if isinstance(dm.font_size, DmFontSize):
+                dm_dict["font_size"] = dm.font_size.value
+            else:
+                dm_dict["font_size"] = dm.font_size
             dm_dict["is_sub"] = dm.is_sub
             dm_dict["pool"] = dm.pool
             dm_dict["attr"] = dm.attr
             dm_list.append(dm_dict)
-    except Exception as e:
-        return HttpResponse({
-            "code": 404, 
-            "message": str(e)
-        }, "application/json", 404)
-    else:
-        return HttpResponse(dm_list, "application/json", 200)
+    #except Exception as e:
+    #    return HttpResponse(json.dumps({
+    #        "code": 404, 
+    #        "message": str(e)
+    #    }), "application/json", 404)
+    #else:
+        return HttpResponse(json.dumps(dm_list), "application/json", 200)
